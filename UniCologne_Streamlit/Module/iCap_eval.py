a6e3ce5f2236b03b52332e84e41c60fcac97dd24
JJPak <100725995+JJPak@users.noreply.github.com>
2023-06-01 16:36:41 +0200

    Update iCap_eval.py
    asdf

--- UniCologne_Streamlit/Module/iCap_eval.py
+++ UniCologne_Streamlit/Module/iCap_eval.py
@@ -157,6 +157,7 @@ def calc_background_cps(df_cps, name_of_BG : str = 'BG', name_of_wash : str= 'HN
         
     # calc mean and sd
     try:
+        st.write(df_cps)
         st.success(f""" 0 """)
         df_cps_mean = pd.Series(dict(df_cps.mean()))        #error somewhere here cannot convert string to float - why is there a string
         st.success(f""" 1 """)
