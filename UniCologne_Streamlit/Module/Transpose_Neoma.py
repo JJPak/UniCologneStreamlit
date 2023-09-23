73d79869ef4e76bc5965eef82cef939672e85dac
JJPak <100725995+JJPak@users.noreply.github.com>
2023-09-23 20:34:11 +0200

    Update Transpose_Neoma.py

--- UniCologne_Streamlit/Module/Transpose_Neoma.py
+++ UniCologne_Streamlit/Module/Transpose_Neoma.py
@@ -21,6 +21,7 @@ def data_crunch_neoma (df, element_choose):
     sample_list2 = []
     sample_list3 = []
     
+    # possible elements and needed infos for them
     if element_choose == 'W':
         element_list_orig = ['Static 1:177Hf', 'Static 1:178Hf', 'Static 1:180W', 'Static 1:181Ta', 'Static 1:182W', 'Static 1:183W',
                          'Static 1:184W','Static 1:186W', 'Static 1:188Os', '180W/184W', '182W/184W', '183W/184W', '186W/184W',
@@ -46,11 +47,12 @@ def data_crunch_neoma (df, element_choose):
     
     
     
-    
+    # add names for empty columns
     df = df.rename(columns = {'Unnamed: 0':'Measurement order', 'Unnamed: 1':'Sample'})
     
     check_elements = df.iloc[[1]].values.tolist()
-
+    
+    #check if all element of interest can be found in df
     # check if element_list_orig in df
     for i in range(len(element_list_orig[0])):
         if element_list_orig[i] not in check_elements[0]:
@@ -68,7 +70,7 @@ def data_crunch_neoma (df, element_choose):
     for i in range(len(element_list_orig)):
         df.replace(element_list_orig[i], element_list_rename[i], inplace = True)
     
-    # # Transpose data frame
+    # Transpose data frame
     df = df.T.reset_index(drop=True)
     
     #  New header
@@ -88,16 +90,18 @@ def data_crunch_neoma (df, element_choose):
     # drop Unit
     df.drop('Unit', axis=1, inplace=True)
     
-    
+    # split the large data frames in small dataframes - you get many dataframes where only one isotope or ratio is init
     for i in element_list_rename:
         df_append = df[df['Isotope']== i ].reset_index(drop=True)
         df_list.append(df_append)
-    
+        
+    # add a row with the name of the isotope
     for frames in df_list:
         name = frames.loc[0,'Isotope']
         row = [name]*len(frames.columns)
         frames.loc[60] = row
-    
+        
+    # take column x from each isotope dataframe so that you get one dataframe for one sample with each isotope and ratio
     lister = []
     for columns in range(len(df_list[0].columns)):
         for frames in df_list:
@@ -106,10 +110,11 @@ def data_crunch_neoma (df, element_choose):
         sample_list.append(df_new)
         lister.clear()
         
+    # delete two unessential columns
     del sample_list[0]
     del sample_list[0]
     
-    
+    # add a column with the name of the sample that is in that df    
     for frames in sample_list:
         column_name = frames.columns[0]
         column_name = [column_name]*len(frames)
@@ -117,33 +122,36 @@ def data_crunch_neoma (df, element_choose):
         column_name.append('Sample')
         frames['Sample'] = column_name
     
-    #  New header
+    # change the header to the name of the isotopes
     for frames in sample_list:
         new_header = frames.iloc[60] #grab the first row for the header
         frames.columns = new_header #set the header row as the df header
 
+    # change all isotopes and ratios that can be found from str to float
     for frames in sample_list:
         frame_2 = frames.drop([60])
         for i in element_list_rename:
             frame_2[i] = frame_2[i].astype(float)
-        
         sample_list2.append(frame_2)
         
           
-    #add other ratios
+    #add ratios that are not yet in the df but that can be calculated
     for i in range(len(missing_ratios_list)):
         for frames in sample_list2:
             frames[missing_ratios_list[i]] = frames[numerator_list[i]] / frames[denominator_list[i]]
             frames['Cycle'] = range(1,61)
             
+    # add elements to the final last to order the elements in the final dataframes
     final_order_list.insert(0, 'Sample')
     final_order_list.insert(1, 'Cycle')
     
+    # add an empty row to all dfs
     for frames in sample_list2:
         frames_new = frames[final_order_list]
         frames_new = append_empty_row(frames_new)
         sample_list3.append(frames_new)
-    
+        
+    # combine all dfs and transpose
     df_new = pd.concat(sample_list3)
     final_df = df_new.T
 
