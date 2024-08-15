10ab0e9968672f38b8937bb97a14f44019f4ed04
JJPak <100725995+JJPak@users.noreply.github.com>
2024-08-15 09:46:24 +0200

    Update Crunch_Neoma_data.py

--- UniCologne_Streamlit/pages/Crunch_Neoma_data.py
+++ UniCologne_Streamlit/pages/Crunch_Neoma_data.py
@@ -51,8 +51,7 @@ if upload_excel is not None and Table_checkbox == True:
 st.subheader('3.1. Choose the element')
 
 element = st.selectbox(
-    'Which element do you choose',
-    ('Hf'))
+    'Which element do you choose', ('Hf',))
 
 st.write('You selected:', element)
 
